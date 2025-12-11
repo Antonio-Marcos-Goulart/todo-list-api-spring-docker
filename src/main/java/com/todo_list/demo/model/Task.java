ba1da7e34f6c3dea3f084c38df2e3a9f578db55e
Antonio Marcos Goulart <antoniomarcos5674335@gmail.com>
2025-12-11 06:25:17 -0300

    Feat: Classe referente as Tasks

---
+++ src/main/java/com/todo_list/demo/model/Task.java
@@ -0,0 +1,35 @@
+package com.todo_list.demo.model;
+
+import com.todo_list.demo.enuns.TaskPriority;
+import com.todo_list.demo.enuns.TaskStatus;
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.Size;
+
+@Entity
+@Table(name = "tasks")
+public class Task {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long taskId;
+
+    @NotEmpty
+    @Size(min = 3, max = 100, message = "The task title must be between 3 and 100 characters")
+    @Column(name = "task_title")
+    private String title;
+
+    @NotEmpty
+    @Size(min = 3, max = 100, message = "The task description must be between 3 and 100 characters")
+    @Column(name = "task_description")
+    private String description;
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = "task_status")
+    private TaskStatus taskStatus;
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = "task_priority")
+    private TaskPriority taskPriority;
+
+}
